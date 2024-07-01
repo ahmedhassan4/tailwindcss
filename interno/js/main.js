ba9880db9bea9017d6dce0eafbc54db6aa3a5956
ahmed hassan <ahmedhsn787@gmail.com>
2024-07-01 08:37:12 +0300

    -

---
+++ interno/js/main.js
@@ -0,0 +1,89 @@
+const navTriggerMenu = document.querySelector("#nav_trigger_btn");
+const navMenu = document.querySelector("#nav_menu");
+
+navTriggerMenu.addEventListener("click", () => {
+  navMenu.classList.toggle("nav-is-open");
+});
+
+// swiper
+const swiper = new Swiper(".swiper", {
+  loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  slidesPerView: 3,
+  spaceBetween: 20,
+  breakpoints: {
+    320: {
+      slidesPerView: 1,
+    },
+    960: {
+      slidesPerView: 2,
+    },
+    1200: {
+      slidesPerView: 3,
+    },
+  },
+});
+
+// scroll Reveal Animations
+const sr = ScrollReveal({
+  origin: "bottom",
+  distance: "60px",
+  duration: 2500,
+  delay: 200,
+});
+
+// hero
+sr.reveal(".hero__text", { origin: "top" });
+
+// steps
+sr.reveal(".steps__step", { distance: "100px", interval: 100 });
+
+// about
+
+sr.reveal(".about__text", { origin: "left" });
+sr.reveal(".about__img", { origin: "right", delay: 800 });
+
+// testimonial
+sr.reveal(".testimonial__bg", { delay: 800 });
+sr.reveal(".testimonial__title");
+sr.reveal(".testimonial__slider", { delay: 1000 });
+
+// brands
+sr.reveal(".brand__img", { delay: 600, distance: "100px", interval: 100 });
+
+// work
+sr.reveal(".work__title");
+sr.reveal(".work__subtitle", { delay: 800 });
+sr.reveal(".work__grid", { delay: 1000 });
+
+// stats
+sr.reveal(".stats");
+sr.reveal(".stats__item", {
+  distance: "100px",
+  interval: 100,
+});
+
+// news
+sr.reveal(".news__title");
+sr.reveal(".news__subtitle", { delay: 800 });
+sr.reveal(".news__item", {
+  distance: "100px",
+  interval: 100,
+  delay: 1000,
+});
+
+// contact
+sr.reveal(".contact__container");
+sr.reveal(".contact__text", { delay: 800 });
+
+// footer
+
+sr.reveal(".footer__item", {
+  distance: "100px",
+  interval: 100,
+});
+
+sr.reveal(".footer__Copyright");
